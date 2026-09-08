13c1d7882ce5ec961a1307038b93f6745b941c29
sabarishh14 <sbsabarish14@gmail.com>
2026-09-08 21:20:08 +0530

    chore(release): bump version to v1.0.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.example.dailytrack_mobile"
         minSdk = 27
         targetSdk = 37
-        versionCode = 10
-        versionName = "1.0.9"
+        versionCode = 11
+        versionName = "1.0.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
